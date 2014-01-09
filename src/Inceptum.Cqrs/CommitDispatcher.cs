128d1beb46e73ccfba2e79e6a32844aed913df98
Mikhail Tripolitov <mtripolitov@inceptumsoft.com>
2014-01-09 10:46:27 +0400

    Add debug logging into CommitDispatcher Dispatch method

--- src/Inceptum.Cqrs/CommitDispatcher.cs
+++ src/Inceptum.Cqrs/CommitDispatcher.cs
@@ -1,12 +1,13 @@
 ﻿using NEventStore;
 using NEventStore.Dispatcher;
+using NLog;
 
 namespace Inceptum.Cqrs
 {
     class CommitDispatcher : IDispatchCommits
     {
         private readonly IEventPublisher m_EventPublisher;
-
+        private readonly Logger m_Logger = LogManager.GetCurrentClassLogger();
         public CommitDispatcher(IEventPublisher eventPublisher)
         {
             m_EventPublisher = eventPublisher;
@@ -18,9 +19,11 @@ namespace Inceptum.Cqrs
 
         public void Dispatch(Commit commit)
         {
+            if (m_Logger.IsDebugEnabled) m_Logger.Debug(string.Format("Stream [{0}] Commit [{1}] dispatching {2} events", commit.StreamId, commit.CommitId, commit.Events.Count));
             foreach (var @event in commit.Events)
             {
                 m_EventPublisher.PublishEvent(@event.Body);
+                if (m_Logger.IsDebugEnabled) m_Logger.Debug(string.Format("Stream [{0}] Commit [{1}] event '{2}' dispatched", commit.StreamId, commit.CommitId, @event.Body.GetType()));
             }
         }
     }
