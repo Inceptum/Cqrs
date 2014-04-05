a2b2870480320b551669bda082245d24958ba7c5
Konstantin Nosov <knosov@inceptumsoft.com>
2014-04-05 18:25:24 +0400

    RabbitMqConventionEndpointResolver keywords for rvrnts and commands

--- src/Inceptum.Cqrs/RabbitMqConventionEndpointResolver.cs
+++ src/Inceptum.Cqrs/RabbitMqConventionEndpointResolver.cs
@@ -13,13 +13,17 @@ namespace Inceptum.Cqrs
         private string m_SerializationFormat;
         private readonly string m_ExclusiveQueuePostfix;
         private readonly string m_EnvironmentPrefix;
+        private static string m_CommandsKeyword;
+        private static string m_EventsKeyword;
 
-        public RabbitMqConventionEndpointResolver(string transport,string serializationFormat,string exclusiveQueuePostfix=null,string environment=null)
+        public RabbitMqConventionEndpointResolver(string transport, string serializationFormat, string exclusiveQueuePostfix = null, string environment = null, string commandsKeyword = null, string eventsKeyword=null)
         {
             m_EnvironmentPrefix = environment!=null?environment+".":"";
             m_ExclusiveQueuePostfix = "." + (exclusiveQueuePostfix ?? Environment.MachineName);
             m_Transport = transport;
             m_SerializationFormat = serializationFormat;
+            m_CommandsKeyword = commandsKeyword;
+            m_EventsKeyword = eventsKeyword;
         }
 
         private string createQueueName(string queue,bool exclusive)
@@ -43,8 +47,8 @@ namespace Inceptum.Cqrs
                 {
                     Destination = new Destination
                     {
-                        Publish = createExchangeName(string.Format("{0}.{1}.exchange/{2}", key.LocalContext, key.RouteType.ToString().ToLower(), rmqRoutingKey)),
-                        Subscribe = createQueueName(string.Format("{0}.queue.{1}.{2}", key.LocalContext, key.RouteType.ToString().ToLower(), queueName),key.Exclusive)
+                        Publish = createExchangeName(string.Format("{0}.{1}.exchange/{2}", key.LocalContext, getKewordByRoutType(key.RouteType), rmqRoutingKey)),
+                        Subscribe = createQueueName(string.Format("{0}.queue.{1}.{2}", key.LocalContext, getKewordByRoutType(key.RouteType), queueName), key.Exclusive)
                     },
                     SerializationFormat = m_SerializationFormat,
                     SharedDestination = true,
@@ -59,7 +63,7 @@ namespace Inceptum.Cqrs
                 {
                     Destination = new Destination
                     {
-                        Publish = createExchangeName(string.Format("{0}.{1}.exchange/{2}", key.RemoteBoundedContext, key.RouteType.ToString().ToLower(), rmqRoutingKey)),
+                        Publish = createExchangeName(string.Format("{0}.{1}.exchange/{2}", key.RemoteBoundedContext, getKewordByRoutType(key.RouteType), rmqRoutingKey)),
                         Subscribe = null
                     },
                     SerializationFormat = m_SerializationFormat,
@@ -74,8 +78,8 @@ namespace Inceptum.Cqrs
                 {
                     Destination = new Destination
                     {
-                        Publish =createExchangeName(string.Format("{0}.{1}.exchange/{2}", key.RemoteBoundedContext, key.RouteType.ToString().ToLower(), key.MessageType.Name)),
-                        Subscribe = createQueueName( string.Format("{0}.queue.{1}.{2}.{3}", key.LocalContext, key.RemoteBoundedContext, key.RouteType.ToString().ToLower(), route),key.Exclusive)
+                        Publish = createExchangeName(string.Format("{0}.{1}.exchange/{2}", key.RemoteBoundedContext, getKewordByRoutType(key.RouteType), key.MessageType.Name)),
+                        Subscribe = createQueueName(string.Format("{0}.queue.{1}.{2}.{3}", key.LocalContext, key.RemoteBoundedContext, getKewordByRoutType(key.RouteType), route), key.Exclusive)
                     },
                     SerializationFormat = m_SerializationFormat,
                     SharedDestination = true,
@@ -90,7 +94,7 @@ namespace Inceptum.Cqrs
                 {
                     Destination = new Destination
                     {
-                        Publish = createExchangeName(string.Format("{0}.{1}.exchange/{2}", key.LocalContext, key.RouteType.ToString().ToLower(), key.MessageType.Name)),
+                        Publish = createExchangeName(string.Format("{0}.{1}.exchange/{2}", key.LocalContext, getKewordByRoutType(key.RouteType), key.MessageType.Name)),
                         Subscribe = null
                     },
                     SerializationFormat = m_SerializationFormat,
@@ -101,6 +105,22 @@ namespace Inceptum.Cqrs
             return default(Endpoint);
 
         }
+
+        private static string getKewordByRoutType(RouteType routeType)
+        {
+            string keyword = null;
+            switch (routeType)
+            {
+                    case RouteType.Commands:
+                        keyword=m_CommandsKeyword;
+                        break;
+                    case RouteType.Events:
+                        keyword=m_EventsKeyword;
+                    break;
+            }
+            return keyword??(routeType.ToString().ToLower());
+        }
+
         public Endpoint Resolve(string route, RoutingKey key, IEndpointProvider endpointProvider)
         {
             lock (m_Cache)
